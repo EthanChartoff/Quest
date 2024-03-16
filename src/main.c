ffe9e4ac2981c474eb883112a726e4c25fef14a4
goodman <chartoff.ethan@gmail,com>
2024-03-16 14:31:09 +0200

    stash

--- src/main.c
+++ src/main.c
@@ -16,42 +16,34 @@ int main(int argc, char* argv[]) {
     
     // compile_file(argv[1]);
     // init_default_dfa();
-    symbol_T *E_s =  init_symbol_non_terminal(init_non_terminal("S'", NON_TERM_start));
-    symbol_T *E = init_symbol_non_terminal(init_non_terminal("S", NON_TERM_S));
-    symbol_T *T = init_symbol_non_terminal(init_non_terminal("E" ,NON_TERM_E));
-    symbol_T *F = init_symbol_non_terminal(init_non_terminal("F", NON_TERM_F));
+    symbol_T *S_s =  init_symbol_non_terminal(init_non_terminal("S'", NON_TERM_start));
+    symbol_T *S = init_symbol_non_terminal(init_non_terminal("S", NON_TERM_S));
+    symbol_T *C = init_symbol_non_terminal(init_non_terminal("C" ,NON_TERM_C));
 
-    symbol_T *p = init_symbol_terminal(init_token("+", TOK_PLUS));
-    symbol_T *x = init_symbol_terminal(init_token("*", TOK_STAR));
-    symbol_T *lp = init_symbol_terminal(init_token("(", TOK_LPAREN));
-    symbol_T *rp = init_symbol_terminal(init_token(")", TOK_RPAREN));
-    symbol_T *id = init_symbol_terminal(init_token("id", TOK_IDENTIFIER));
+    symbol_T *d = init_symbol_terminal(init_token("d", TOK_DO));
+    symbol_T *c = init_symbol_terminal(init_token("c", TOK_CASE));
 
-    symbol_T *EpT[3] = {T, p, E};
-    symbol_T *TxF[3] = {F, x, T};
-    symbol_T *l_E_r[3] = {lp, E, rp};
+    symbol_T *CC[] = {C, C};
+    symbol_T *cC[] = {c, C};
 
-    rule_T *start_r = init_rule(E_s->symbol->non_terminal, &E, 1);
-    rule_T *EtoEpT = init_rule(E->symbol->non_terminal, EpT, 3);
-    rule_T *EtoT = init_rule(E->symbol->non_terminal, &T, 1);
-    rule_T *TtoTxF = init_rule(T->symbol->non_terminal, TxF, 3);
-    rule_T *TtoF = init_rule(T->symbol->non_terminal, &F, 1);
-    rule_T *E_paren = init_rule(F->symbol->non_terminal, l_E_r, 1);
-    rule_T *idr = init_rule(F->symbol->non_terminal, &id, 1);
+    rule_T *start = init_rule(S_s->symbol->non_terminal, &S, 1);
+    rule_T *SCC = init_rule(S->symbol->non_terminal, CC, 2);
+    rule_T *CcC = init_rule(C->symbol->non_terminal, cC, 2);
+    rule_T *Cd = init_rule(C->symbol->non_terminal, &d, 1);
 
 
-    rule_T *rules[] = {start_r, EtoEpT, EtoT, TtoF, idr, TtoTxF, E_paren}; 
-    symbol_T *symbols[] = {E_s, E, T, F, p, x, lp, rp, id};
+    rule_T *rules[] = {start, SCC, CcC, Cd}; 
+    symbol_T *symbols[] = {S_s, S, C, d, c};
 
     grammer_T gram = {
         rules,
-        7,
+        4,
         symbols,
-        9
+        5
     };
 
 
-    lr_item_set_T *itms = lr1_items(&gram, init_lr_item(start_r, 0, init_token("eof", TOK_eof)));
+    lr_item_set_T *itms = lr1_items(&gram, init_lr_item(start, 0, init_token("eof", TOK_eof)));
     
     for(int i = 0; i < itms->size; ++i) {
         printf("%zu - %s, %s\n", itms->set[i]->dot_index, itms->set[i]->rule->left->value, itms->set[i]->lookahead->value);
